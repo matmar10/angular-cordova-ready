f73fa1585da682d58e79f5169d3a0b41915603cd
Matthew J. Martin <Matthew.Martin@monitise.com>
2014-04-02 16:25:53 -0700

    be sure to fire ready event even when not using cordova simulator

--- src/angular-cordova-ready.js
+++ src/angular-cordova-ready.js
@@ -9,19 +9,16 @@
             cordovaReady = $q.defer(),
             cordovaReadyPromise = cordovaReady.promise;
 
-        // if we're not on a device, simulate the cordovaready event
-        if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-            // TODO: remove jquery dependency
-            $document.on('ready', function () {
-                $document.triggerHandler('deviceready');
-            });
-        }
-
         $document.on('deviceready', cordovaReady.resolve);
 
-        // cordova already loaded
-        if (window.device) {
+        // if we're not on a device, simulate the cordovaready event
+        if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
             $document.triggerHandler('deviceready');
+        } else {
+            // cordova already loaded
+            if (window.device) {
+                $document.triggerHandler('deviceready');
+            }
         }
 
         /**
@@ -42,3 +39,4 @@
     });
 
 })();
+
